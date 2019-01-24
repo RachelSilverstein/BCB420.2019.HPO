37383a9b53c0df9d38b232e1e44a864b62a7259d
Rachel Silverstein <Rachel@Rachels-MacBook-Pro-2.local>
2019-01-23 19:41:11 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# BCB420.2019.HPO
-Human Phenotype Ontology Package
+# `BCB420.2019.HPO`
+
+#### (Human Phenotype Ontology Package)
+
+
+&nbsp;
+
+###### [Rachel Silverstein](https://orcid.org/0000-0001-5724-2252), &lt;rachel.silverstein@mail.utoronto.ca&gt;
+
+<!--end-->
---
+++ dev/functionTemplate.R
@@ -0,0 +1,31 @@
+# functionTemplate.R
+
+#' Title.
+#'
+#' \code{<function>} description.
+#'
+#' Details.
+#' @section <title>: Additional explanation.
+#'
+#' @param <p> <description>.
+#' @param <q> <description>.
+#' @return <description>.
+#'
+#' @family <optional description of family>
+#'
+#' @author \href{https://orcid.org/0000-0002-1134-6758}{Boris Steipe} (aut)
+#'
+#' @seealso \code{\link{<function>}} <describe related function>, ... .
+#'
+#' @examples
+#' # <explain what the example does>
+#' myFunction(1i, 1i)
+#'
+#' @export
+
+myFunction <- function(p, q) {
+
+    return(p * q)
+}
+
+# [END]
